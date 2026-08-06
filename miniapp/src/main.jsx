af7bd72eeeac6e3ff1d605adcbde632bc4dfc2e0
aemccave-ui <a.e.mccave@gmail.com>
2026-08-06 20:45:24 +0300

    frontend: load account distribution styles

--- miniapp/src/main.jsx
+++ miniapp/src/main.jsx
@@ -5,6 +5,7 @@ import '../packages/lab-design-system/navigation.css'
 import './styles.css'
 import './polish.css'
 import './currency-layout.css'
+import './account-distribution.css'
 import './currency-summary.js'
 
 createRoot(document.getElementById('root')).render(
