e01b7378b3645901caabb41e66f0fbe900af2dc0
aemccave-ui <a.e.mccave@gmail.com>
2026-08-12 03:17:43 +0300

    feat(ux022r3): mount quick operation portal

--- miniapp/src/main.jsx
+++ miniapp/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import QuickOperationPortal from './QuickOperationPortal.jsx'
 import '../packages/lab-design-system/navigation.css'
 import './styles.css'
 import './polish.css'
@@ -18,5 +19,6 @@ import './quick-actions-runtime.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <QuickOperationPortal />
   </StrictMode>,
 )
